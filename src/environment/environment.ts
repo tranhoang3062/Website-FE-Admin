7d700f7f3bb55a3f30d02af525141f89181a05e7
Nguyen Tran Hoang <hoangnguyen@interspace.vn>
2024-05-18 15:27:47 +0700

    init

---
+++ src/environment/environment.ts
@@ -0,0 +1,12 @@
+export const domain = 'http://localhost:4200';
+export const environment = {
+  production: true,
+  appVersion: '8.0.0',
+  apiUrl: 'https://hq3-api.dev.accesstrade.me',
+  domain: domain,
+//   google_client_id: '272295239521-skgn1nokbff9gv4fbqvjpm7a9p1ba8ti.apps.googleusercontent.com',
+//   google_redirect_url: domain + '/auth/google-login',
+//   tokenField: 'jwt',
+//   livestreamUrl: 'https://templates.staging.bluesea.live',
+//   livestreamGateway: 'https://gateway.staging.bluesea.live',
+};
